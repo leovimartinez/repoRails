087fcaea109599974b41a7a9451b0084969b60ce
leovimartinez <leovi.martinez@softtek.com>
2016-07-29 20:25:26 -0500

    navbar

--- wokspaceRails/ejercicioRails/config/routes.rb
+++ wokspaceRails/ejercicioRails/config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'paginas/pag2'
 
+  root 'paginas#pag1'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
